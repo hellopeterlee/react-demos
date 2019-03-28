c26a614a378413b9b7888c09864eecc7b8b8d73c
peirenlei <peirenlei@163.com>
2019-03-28 13:44:05 +0800

    react-demos

---
+++ config/webpack.base.conf.js
@@ -0,0 +1,78 @@
+'use strict'
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  // 入口起点
+  entry: {
+    demo01: './demo01/app.js',
+  },
+  // 输出
+  output: {
+    path: path.resolve(__dirname, '../dist'),
+    filename: "[name].js",
+  },
+  // 解析
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.json'],
+    alias: {
+      '@': path.join(__dirname, '..', 'src')
+    }
+  },
+  // loader
+  module: {
+    rules: [
+      {
+        test: /\.js|jsx$/,
+        exclude: /(node_modules|bower_components)/,// 屏蔽不需要处理的文件（文件夹）（可选）
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          name: 'images/[name].[hash:7].[ext]'
+        }
+      },
+      {
+        test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          name: 'fonts/[name].[hash:7].[ext]'
+        }
+      },
+      {
+        test: /\.css|scss$/,
+        use: [
+          { loader: 'style-loader' },
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+              localIdentName: '[path][name]__[local]--[hash:base64:5]'
+            }
+          },
+          { loader: 'sass-loader',
+            options: {
+              modules: true,
+              localIdentName: '[path][name]__[local]--[hash:base64:5]'
+            }
+          }
+        ]
+      }
+    ]
+  },
+  // 目标运行环境
+  target: "web",
+  // 插件
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'demo01/index.html',
+      template: 'demo01/index.html',
+      inject: 'body'
+    })
+  ]
+};
---
+++ demo01/app.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import App from './App.js';
+
+// ReactDOM.render(<App />, document.getElementById('app'));
+
+ReactDOM.render(
+	<div><h1>helloworld</h1></div>, 
+	document.getElementById('app')
+);
